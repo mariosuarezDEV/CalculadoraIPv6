9a7b0d4bbae22b76b53fe6918e558696f471d466
Luis Mario Cervantes Suarez <zs21015977@estudiantes.uv.mx>
2023-11-28 14:02:08 -0600

    La regla 1 ya funciona

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import flet as ft
+import regla1 as r1
 
 def main(page: ft.Page):
     page.title = "Calculadora IPv6"
+
     
     #CAMBIAR TEMA DE LA APP
+    
     page.window_resizable=True #no permitir la resizable
     page.theme_mode=ft.ThemeMode.DARK #cambiar el tema de la aplicación
     page.window_width=470 #tamaño de ancho
@@ -26,7 +29,29 @@ def main(page: ft.Page):
         destino_navegacion = e.control.selected_index #Esto almacena el indice del boton que presiona el usuario
         
         #controlamos los casos
-        if destino_navegacion == 0:
+        if destino_navegacion == 0: #regla 1
+            #pedir la direccion ipv6
+            tb1 = ft.TextField(label="Dirección IPv6")
+            page.add(tb1) #agregar el textfield 
+            #agregar un espacio para que el boton y el textfield tengan una separación y no se vean pegados
+            page.add(ft.Text(value="\n"))
+            #funcion de calcular la ip
+            def mostrar_respuesta(e):
+                direccion_con_regla_uno = r1.regla1(tb1.value)
+                
+                dlg = ft.AlertDialog(
+                    title=ft.Text(direccion_con_regla_uno)
+                )
+                
+                page.dialog = dlg
+                dlg.open = True
+                page.update()
+
+            #agregar el boton para calcular
+            boton_enviar = ft.ElevatedButton("Calcular la dirección", height=60, on_click=mostrar_respuesta)
+            page.add(boton_enviar) #agregar el boton para calcular la direccion ip (esto hace la funcionalidad)
+            
+        elif destino_navegacion == 1: #regla 2
             #pedir la direccion ipv6
             tb1 = ft.TextField(label="Dirección IPv6")
             page.add(tb1) #agregar el textfield 
@@ -36,17 +61,7 @@ def main(page: ft.Page):
             boton_enviar = ft.ElevatedButton("Calcular la dirección", height=60)
             page.add(boton_enviar) #agregar el boton para calcular la direccion ip (esto hace la funcionalidad)
             
-        elif destino_navegacion == 1:
-            #pedir la direccion ipv6
-            tb1 = ft.TextField(label="Dirección IPv6")
-            page.add(tb1) #agregar el textfield 
-            #agregar un espacio para que el boton y el textfield tengan una separación y no se vean pegados
-            page.add(ft.Text(value="\n"))
-            #agregar el boton para calcular
-            boton_enviar = ft.ElevatedButton("Calcular la dirección", height=60)
-            page.add(boton_enviar) #agregar el boton para calcular la direccion ip (esto hace la funcionalidad)
-            
-        elif destino_navegacion == 2:
+        elif destino_navegacion == 2: #ambas reglas
             #pedir la direccion ipv6
             tb1 = ft.TextField(label="Dirección IPv6")
             page.add(tb1) #agregar el textfield 
